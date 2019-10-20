4b28020fe32d9dc66b49bd9390324c3d87106731
xingkongdadao <xingkongdadao@gmail.com>
2019-10-20 12:37:31 -0700

    第10次build， 从后端调取数据

--- src/app/modules/admin/pages/home/home.component.ts
+++ src/app/modules/admin/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  private URL = 'http://111.231.101.150:8801';
+  goodsCategory: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+
+    this.getGoodsCategoryList().subscribe(
+      value => {
+        this.goodsCategory = value.results;
+      }
+    );
+
   }
 
-}
+  // 提取所有商品类别
+  getGoodsCategoryList(page = 1 , pageSize = 20 , goodsCategory = ''): Observable<any> {
+
+    const params = new HttpParams()
+      .set('name_contains', goodsCategory.toString().trim().toLowerCase() )
+      .set('page', page.toString() )
+      .set('page_size', pageSize.toString() );
+
+    return this.http.get<any>(this.URL + '/goods_category/', {params});
+  }}
